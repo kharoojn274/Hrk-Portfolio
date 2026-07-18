cdf89afc956197159cbb62a866c17c8aff13a3d3
Haroon Rasheed <kharoon274@gmail.com>
2026-07-18 17:15:30 +0300

    settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,6 +25,8 @@ ALLOWED_HOSTS = config(
 CORS_ORIGIN_ALLOW_ALL = True # If this is used then `CORS_ORIGIN_WHITELIST` will not have any effect
 CORS_ALLOW_CREDENTIALS = True
 
+WHITENOISE_MANIFEST_STRICT = False
+
 LOGIN_URL = '/dashboard/login/'
 LOGOUT_URL = '/dashboard/logout/'
 
